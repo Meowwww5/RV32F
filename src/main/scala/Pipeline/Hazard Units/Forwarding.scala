205e4763a3c133120bf6853db28630f5e551c1a0
Meowwww5 <148947567+Meowwww5@users.noreply.github.com>
2025-01-21 17:42:32 +0000

    update: add forward_c

--- src/main/scala/Pipeline/Hazard Units/Forwarding.scala
+++ src/main/scala/Pipeline/Hazard Units/Forwarding.scala
@@ -2,7 +2,7 @@ package Pipeline
 
 import chisel3._
 import chisel3.util._
-//add rs3?
+
 class Forwarding extends Module {
     val io = IO(new Bundle {
         val IDEX_rs1 = Input(UInt(5.W))
@@ -15,16 +15,24 @@ class Forwarding extends Module {
         
         val forward_a = Output(UInt(2.W))
         val forward_b = Output(UInt(2.W))
+        //RV32F rs3
+        val forward_c = Output(UInt(2.W))
     })
 
     io.forward_a := "b00".U
     io.forward_b := "b00".U
+    io.forward_c := "b00".U
 
     // EX HAZARD
     when(io.EXMEM_regWr === "b1".U && io.EXMEM_rd =/= "b00000".U && 
-            (io.EXMEM_rd === io.IDEX_rs1.asUInt) && (io.EXMEM_rd === io.IDEX_rs2)) {
+            (io.EXMEM_rd === io.IDEX_rs1.asUInt) && (io.EXMEM_rd === io.IDEX_rs2) && (io.EXMEM_rd === io.IDEX_rs3)) {
         io.forward_a := "b10".U
         io.forward_b := "b10".U
+        io.forward_c := "b10".U
+
+    }.elsewhen(io.EXMEM_regWr === "b1".U && io.EXMEM_rd =/= "b00000".U && 
+            (io.EXMEM_rd === io.IDEX_rs3)) {    
+        io.forward_c := "b10".U
 
     }.elsewhen(io.EXMEM_regWr === "b1".U && io.EXMEM_rd =/= "b00000".U && 
             (io.EXMEM_rd === io.IDEX_rs2)) {    
@@ -34,12 +42,31 @@ class Forwarding extends Module {
             (io.EXMEM_rd === io.IDEX_rs1)) {    
         io.forward_a := "b10".U
     }
+    // when(io.EXMEM_regWr === "b1".U && io.EXMEM_rd =/= "b00000".U && 
+    //         (io.EXMEM_rd === io.IDEX_rs1.asUInt) && (io.EXMEM_rd === io.IDEX_rs2)) {
+    //     io.forward_a := "b10".U
+    //     io.forward_b := "b10".U
+
+    // }.elsewhen(io.EXMEM_regWr === "b1".U && io.EXMEM_rd =/= "b00000".U && 
+    //         (io.EXMEM_rd === io.IDEX_rs2)) {    
+    //     io.forward_b := "b10".U
+    
+    // }.elsewhen(io.EXMEM_regWr === "b1".U && io.EXMEM_rd =/= "b00000".U && 
+    //         (io.EXMEM_rd === io.IDEX_rs1)) {    
+    //     io.forward_a := "b10".U
+    // }
 
     // MEM HAZARD
     when((io.MEMWB_regWr === "b1".U) && (io.MEMWB_rd =/= "b00000".U) && (io.MEMWB_rd === io.IDEX_rs1) && (io.MEMWB_rd === io.IDEX_rs2) && 
-            ~(io.EXMEM_regWr === "b1".U && io.EXMEM_rd =/= "b00000".U && (io.EXMEM_rd === io.IDEX_rs1) && (io.EXMEM_rd === io.IDEX_rs2))) {
+        ~(io.EXMEM_regWr === "b1".U && io.EXMEM_rd =/= "b00000".U &&
+        (io.EXMEM_rd === io.IDEX_rs1) && (io.EXMEM_rd === io.IDEX_rs2) && (io.EX_MEM_RD === io.IDEX_rs3)) ) {
         io.forward_a := "b01".U
         io.forward_b := "b01".U
+        io.forward_c := "b01".U
+
+    }.elsewhen((io.MEMWB_regWr === "b1".U) && (io.MEMWB_rd =/= "b00000".U) && (io.MEMWB_rd === io.IDEX_rs3) && 
+            ~(io.EXMEM_regWr === "b1".U && io.EXMEM_rd =/= "b00000".U && (io.EXMEM_rd === io.IDEX_rs3))){
+        io.forward_c := "b01".U
 
     }.elsewhen((io.MEMWB_regWr === "b1".U) && (io.MEMWB_rd =/= "b00000".U) && (io.MEMWB_rd === io.IDEX_rs2) && 
             ~(io.EXMEM_regWr === "b1".U && io.EXMEM_rd =/= "b00000".U && (io.EXMEM_rd === io.IDEX_rs2))){
@@ -48,8 +75,19 @@ class Forwarding extends Module {
     }.elsewhen((io.MEMWB_regWr === "b1".U) && (io.MEMWB_rd =/= "b00000".U) && (io.MEMWB_rd === io.IDEX_rs1) && 
             ~(io.EXMEM_regWr === "b1".U && io.EXMEM_rd =/= "b00000".U && (io.EXMEM_rd === io.IDEX_rs1))){
         io.forward_a := "b01".U
-        }
+    }    
+    // when((io.MEMWB_regWr === "b1".U) && (io.MEMWB_rd =/= "b00000".U) && (io.MEMWB_rd === io.IDEX_rs1) && (io.MEMWB_rd === io.IDEX_rs2) && 
+    //         ~(io.EXMEM_regWr === "b1".U && io.EXMEM_rd =/= "b00000".U && (io.EXMEM_rd === io.IDEX_rs1) && (io.EXMEM_rd === io.IDEX_rs2))) {
+    //     io.forward_a := "b01".U
+    //     io.forward_b := "b01".U
+
+    // }.elsewhen((io.MEMWB_regWr === "b1".U) && (io.MEMWB_rd =/= "b00000".U) && (io.MEMWB_rd === io.IDEX_rs2) && 
+    //         ~(io.EXMEM_regWr === "b1".U && io.EXMEM_rd =/= "b00000".U && (io.EXMEM_rd === io.IDEX_rs2))){
+    //     io.forward_b := "b01".U
+
+    // }.elsewhen((io.MEMWB_regWr === "b1".U) && (io.MEMWB_rd =/= "b00000".U) && (io.MEMWB_rd === io.IDEX_rs1) && 
+    //         ~(io.EXMEM_regWr === "b1".U && io.EXMEM_rd =/= "b00000".U && (io.EXMEM_rd === io.IDEX_rs1))){
+    //     io.forward_a := "b01".U
+    //     }
     
 }
-
-//Q: rs3??
