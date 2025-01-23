a9cd5cb75c523f2fc994a4cc43d297be5fe4f4ed
Meowwww5 <148947567+Meowwww5@users.noreply.github.com>
2025-01-23 20:31:14 +0800

    add rs3detect

--- src/main/scala/Pipeline/Hazard Units/HazardDetection.scala
+++ src/main/scala/Pipeline/Hazard Units/HazardDetection.scala
@@ -9,6 +9,7 @@ class HazardDetection extends Module {
     val ID_EX_rd = Input(UInt(5.W))
     val pc_in = Input(SInt(32.W))
     val current_pc = Input(SInt(32.W))
+    val ID_EX_operandC = Input(Bool())
 
     val inst_forward = Output(Bool())
     val pc_forward = Output(Bool())
@@ -20,8 +21,10 @@ class HazardDetection extends Module {
 
   val Rs1 = io.IF_ID_inst(19, 15)
   val Rs2 = io.IF_ID_inst(24, 20)
-
-  when(io.ID_EX_memRead === 1.B && ((io.ID_EX_rd === Rs1) || (io.ID_EX_rd === Rs2))) {
+  val Rs3 = io.IF_ID_inst(31, 27)
+  val rs3detect = io.ID_EX_operandC & (io.ID_EX_rd === Rs3)
+  
+  when(io.ID_EX_memRead === 1.B && ((io.ID_EX_rd === Rs1) || (io.ID_EX_rd === Rs2) || rs3detect)) {
     io.inst_forward := true.B
     io.pc_forward := true.B
     io.ctrl_forward := true.B
