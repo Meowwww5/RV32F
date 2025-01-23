83d632b1d77835fb501b01155b339c7a2d2691de
Meowwww5 <148947567+Meowwww5@users.noreply.github.com>
2025-01-24 02:01:29 +0800

    Update annotation L416

--- src/main/scala/Pipeline/UNits/FPU.scala
+++ src/main/scala/Pipeline/UNits/FPU.scala
@@ -413,7 +413,7 @@ class FPU extends Module{
           result := Mux((COMP_20_ && COMP_21_), 1.U, 0.U) //A>=B && B>=A -> A=B
         }
         is(1.U) {
-          result := Mux(COMP_20_, 0.U, 1.U) //A>=B -> A>B
+          result := Mux(COMP_20_, 0.U, 1.U) //!A>=B -> A<B
         }
         is(2.U) {
           result := Mux(COMP_21_, 0.U, 1.U) //B>=A -> A<=B
